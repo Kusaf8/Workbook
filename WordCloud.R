9804b7db6b4bdb3e41dbe1bb6e468371707d155b
Kusaf8 <kevin.a.roberts8@gmail.com>
2023-01-30 18:39:57 +0100

    Add Counter to webpage

--- WordCloud.R
+++ WordCloud.R
@@ -9,6 +9,7 @@ library(RColorBrewer)
 library(tm)
 library(textreadr)
 library(tibble)
+library(dplyr)
 
 articles = read_document("C:\\Users\\Kevin\\Desktop\\CodePractice\\ResumeProject\\Workbook\\docs\\EvaluationWords.docx")
 docs = VCorpus(VectorSource(articles))
@@ -18,7 +19,7 @@ remove(articles)
 transform.words = content_transformer(function(x, from, to) gsub(from, to, x))
 temp = tm_map(docs, transform.words, "<.+?>", " ")
 temp = tm_map(temp, transform.words, "\t", " ")
-temp = tm_map(temp, content_transformer(tolower)) # Conversion to Lowercase
+temp = tm_map(temp, content_transformer(tolower))
 temp = tm_map(temp, PlainTextDocument)
 temp = tm_map(temp, stripWhitespace)
 temp = tm_map(temp, removeWords, stopwords("english"))
@@ -30,21 +31,19 @@ remove(docs)
 dtm = TermDocumentMatrix(temp)
 matrix = as.matrix(dtm)
 words = sort(rowSums(matrix),decreasing=TRUE) 
-df = data.frame(word = names(words),freq=words)
+df = data.frame(word=names(words),freq=words)
+df = df %>% filter(!word %in% c('nga', 'amn','mbrs','mbr', 'dia', 'hrs', 'dao', 'days', 'rpts', 'nst'))
+df$word = toupper(df$word)
 
 set.seed(1234) # for reproducibility 
-png("wordcloud_packages.png", width=12,height=8, units='in', res=300)
+png("wordcloud_packages.png", width=12,height=10, units='in', res=300)
+dev.off()
 
-wc = wordcloud(words = df$word,
+wordcloud(words = df$word,
           freq = df$freq, 
-          min.freq = 2,
-          #max.words=100, 
+          min.freq = 1,
+          max.words=200, 
           random.order=FALSE, 
           rot.per=0.35,
-          scale=c(2,.1),
-          use.r.layout=FALSE,
+          scale=c(2.0,.05),
           colors = brewer.pal(8, "Dark2"))
-
-
-
-wc
